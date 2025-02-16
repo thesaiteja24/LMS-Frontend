9a96da62be89e71811d3df614544479415b493b2
TheSaiTeja <iamsaiteja2411@gmail.com>
2025-02-16 17:45:41 +0530

    Code base improvements and exam analysis intigration

--- src/Student/Exams_module/students/ExamModule/OnlineCompiler.jsx
+++ src/Student/Exams_module/students/ExamModule/OnlineCompiler.jsx
@@ -4,7 +4,7 @@ import { javascript } from "@codemirror/lang-javascript";
 import { python } from "@codemirror/lang-python";
 import { java } from "@codemirror/lang-java";
 import { oneDark } from "@codemirror/theme-one-dark";
-import { ExamContext } from "./ExamModule/ExamContext";
+import { ExamContext } from "./ExamContext";
 
 const OnlineCompiler = () => {
   // Get context values
