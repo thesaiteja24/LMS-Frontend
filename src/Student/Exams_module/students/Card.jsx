162a6ae69df8b13f7f5a10aab967f12f937d04a0
TheSaiTeja <iamsaiteja2411@gmail.com>
2025-02-17 16:41:33 +0530

    update exam dashboard styling'

--- src/Student/Exams_module/students/Card.jsx
+++ src/Student/Exams_module/students/Card.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 export const Card = ({ children, className }) => {
   return (
-    <div className={`bg-white shadow-md rounded-lg p-4 ${className}`}>
+    <div className={`bg-white rounded-lg ${className} shadow-md`}>
       {children}
     </div>
   );
 };
 
 export const CardHeader = ({ children }) => (
-  <div className="border-b pb-2 mb-2">{children}</div>
+  <div className="bg-[#132EE0] text-white font-semibold text-xl p-1 rounded-t-lg">
+    {children}
+  </div>
 );
 
 export const CardTitle = ({ children }) => (
