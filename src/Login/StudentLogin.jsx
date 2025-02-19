2076d8583245e6f646a6543c86fb1bbfdfbfce4e
Mrsmartboy <josephganjela@gmail.com>
2025-02-19 15:36:01 +0530

    studentLo0gin

--- src/Login/StudentLogin.jsx
+++ src/Login/StudentLogin.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Eye, EyeOff } from 'lucide-react';
 import axios from 'axios';
-import Cookies from 'js-cookie';
+// import Cookies from 'js-cookie';
 import './StudentLogin.css';
 import { useUniqueBatches } from '../contexts/UniqueBatchesContext';
 
